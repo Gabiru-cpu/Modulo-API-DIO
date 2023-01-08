3f670dd8e34e10e4a8cd71332086c44918ad8f53
Gabiru-cpu <gabriel_librande@hotmail.com>
2023-01-08 17:06:58 -0300

    API-Apresentar e Data e Hora

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using MODULOAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AgendaContext>(FileOptions =>
+    FileOptions.UseSqlServer(builder.Configuration.GetConnectionString("ConectionDefault")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Primeiro contato com API's
+
+## Comando para instalar o Entity Framework e usar no seus projetos:
+
+### dotnet tool install --global dotnet-ef   OBS: usar apenas uma vez pois é global
+
+### dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+### dotnet add package Microsoft.EntityFrameworkCore.Design   OBS: Toda vez que quiser usar o entity em um projeto
